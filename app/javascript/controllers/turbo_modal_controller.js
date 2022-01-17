cc71eaaf2f10f9e22fea73434927910589e50fae
Yaro <yshmarov@gmail.com>
2022-01-17 10:30:29 +0000

    stimulus - handle turbo form submission

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -5,4 +5,12 @@ export default class extends Controller {
     this.element.parentElement.removeAttribute("src")
     this.element.remove()
   }
+
+  // hide modal on successful form submission
+  // action: "turbo:submit-end->turbo-modal#submitEnd"
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideModal()
+    }
+  }
 }
