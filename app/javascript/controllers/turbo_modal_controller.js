afff027aee96ceedac6243fd91f5ba176af1a81f
Yaro <yshmarov@gmail.com>
2022-01-17 11:10:54 +0000

    close modal on click background or ESC

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -1,9 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["modal"]
+
+  // hide modal
+  // action: "turbo-modal#hideModal"
   hideModal() {
     this.element.parentElement.removeAttribute("src")
-    this.element.remove()
+    // Remove src reference from parent frame element
+    // Without this, turbo won't re-open the modal on subsequent click
+    this.modalTarget.remove()
   }
 
   // hide modal on successful form submission
@@ -12,5 +18,22 @@ export default class extends Controller {
     if (e.detail.success) {
       this.hideModal()
     }
+  }
+
+  // hide modal when clicking ESC
+  // action: "keyup@window->turbo-modal#closeWithKeyboard"
+  closeWithKeyboard(e) {
+    if (e.code == "Escape") {
+      this.hideModal()
+    }
+  }
+
+  // hide modal when clicking outside of modal
+  // action: "click@window->turbo-modal#closeBackground"
+  closeBackground(e) {
+    if (e && this.modalTarget.contains(e.target)) {
+      return;
+    }
+    this.hideModal()
   }
 }
