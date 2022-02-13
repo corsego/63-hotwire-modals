891f148cac72a6d6f94af10b3304639dae5d671b
Yaro <yshmarov@gmail.com>
2022-02-13 14:17:33 +0000

    update ruby and rails version

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.0.3"
+ruby "3.1.0"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.1"
+gem "rails", "~> 7.0.2"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
@@ -74,4 +74,4 @@ group :test do
   gem "webdrivers"
 end
 
-gem "view_component", "~> 2.48"
+gem "view_component"
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_153702) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_08_153702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
